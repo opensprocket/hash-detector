9eecc8d0ca78693ea4e6cd89a631c4be968763ce
opensprocket <opensprocket@users.noreply.github.com>
2024-11-04 17:23:26 -0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -10,13 +10,7 @@ def md5(file_path):
 
 def main():
     args = sys.argv
-    print(f'args: {args}')
-    
-    # if len(args) < 2:
-    #     del args[0] # remove the calling python script
-    # else: 
-    #     print(f'Invalid arguments! Must provide arguments!')
-    #     return
+    # print(f'args: {args}')
     
     result = md5(args[1])
     print(result)
